75258a23499d362dd51ed2483d9e6ef2af17974a
jespe <jesperkauppinen@gmail.com>
2021-12-09 16:47:39 +0200

    smth

--- api/endpoints/messages.py
+++ api/endpoints/messages.py
@@ -1,9 +1,29 @@
-from fastapi import APIRouter, websockets
+from fastapi import APIRouter, websockets, Depends, WebSocket
+from typing import List
 from fastapi.responses import HTMLResponse
-import asyncio
 
+from sqlalchemy.orm import Session
+
+from core.message import get_messages
+from api.deps import get_db
+from core.models import Message
 app = APIRouter()
 
+
+class ConnectionManager:
+    def __init__(self):
+        self.connections: List[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.connections.append(websocket)
+
+    async def broadcast(self, data: str):
+        for connection in self.connections:
+            await connection.send_text(data)
+
+
+manager = ConnectionManager()
 
 html = """
 <!DOCTYPE html>
@@ -13,45 +33,68 @@ html = """
     </head>
     <body>
         <h1>WebSocket Chat</h1>
-        <form action="" onsubmit="sendMessage(event)">
+        <button onClick="showForm(event)" id="connect">Connect</button>
+        <form action="" onsubmit="sendMessage(event)" id="form" style="display: none">
             <input type="text" id="messageText" autocomplete="off"/>
             <button>Send</button>
         </form>
         <ul id='messages'>
         </ul>
         <script>
-            var ws = new WebSocket("ws://localhost:8000/api/msg/ws");
-            ws.onmessage = function(event) {
+            var clientID = Date.now();
+            var ws = new WebSocket(`ws://localhost:8000/ws/${clientID}`);
+
+            function processMessage(event) {
                 var messages = document.getElementById('messages')
                 var message = document.createElement('li')
                 var content = document.createTextNode(event.data)
-                message.appendChild(content)
-                messages.appendChild(message)
-            };
+                message.appendChild(content);
+                messages.appendChild(message);
+            }
+
+            ws.onmessage = processMessage;
+
             function sendMessage(event) {
                 var input = document.getElementById("messageText")
-                ws.send(input.value)
+                var message = document.createElement('li')
+                var content = document.createTextNode(input.value)
+                message.appendChild(content);
+                messages.appendChild(message);
+                ws.send(input.value);
+
                 input.value = ''
                 event.preventDefault()
             }
+
+            function showForm(event) {
+                var button = document.getElementById("connect");
+                var form = document.getElementById("form");
+                button.style.display = "none";
+                form.style.display = "block";
+            }
+
         </script>
     </body>
 </html>
+
 """
 
 list_of_clients = []
 
 
 @app.get("/")
-async def get():
+async def get(db: Session = Depends(get_db)):
+    messages = get_messages(db=db)
+    print(messages)
     return HTMLResponse(html)
 
 
-@app.websocket("/ws")
-async def websocket_endpoint(websocket: websockets.WebSocket):
+@app.websocket("/send")
+async def websocket_endpoint(websocket: websockets.WebSocket, db: Session = Depends(get_db)):
     await websocket.accept()
     while True:
         data = await websocket.receive_text()
-        await websocket.send_text(f"Message text was: {data}")
+        await manager.broadcast(f"Client Jeb: {data}")
+        # await websocket.send_text(f"Message text was: {data}")
 
 
